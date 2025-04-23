8e06fe2a32c97f8c633b222f58ade448b981a9e3
Satyam Mishra <143482705+Satyam940@users.noreply.github.com>
2025-04-23 05:28:56 +0000

    password

--- face/fr/forms.py
+++ face/fr/forms.py
@@ -4,4 +4,4 @@ from .models import KnownFace
 class KnownFaceForm(forms.ModelForm):
     class Meta:
         model = KnownFace
-        fields = ['name', 'uid', 'class_name']
+        fields = ['name', 'uid', 'class_name', 'password']
--- face/fr/models.py
+++ face/fr/models.py
@@ -1,4 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+# models.py
+from django.db import models
 
 class KnownFace(models.Model):
     name = models.CharField(max_length=100)
@@ -6,8 +10,19 @@ class KnownFace(models.Model):
     class_name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='known_faces/', null=True, blank=True)
     encoding = models.TextField()
+    password = models.CharField(max_length=255)  # Added password field
 
     def __str__(self):
         return self.name
 
 
+
+
+class Attendance(models.Model):
+    student = models.ForeignKey(KnownFace , on_delete=models.CASCADE)
+    subject = models.CharField(max_length=100)
+    date =  models.DateField(auto_now=True)
+    present = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.student.name
